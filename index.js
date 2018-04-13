1f2ffd7723e9616663e1e7b61386fe332ab2d7d6
xianwen.yu <xianwen.yu@wmand.me>
2018-04-13 19:22:48 +0800

    融合当前web前端开发模式

---
+++ index.js
@@ -0,0 +1,30 @@
+import './src/css/style.scss'
+import jquery from './src/js/jquery'
+
+require('./src/js/jcarousel')
+require('./src/js/jqzoom')
+$(document).ready(function() {
+    $("#mycarousel").jcarousel({initCallback:mycarousel_initCallback});
+    $(".jqzoom").jqueryzoom({
+         xzoom:420,
+         yzoom:420,
+         offset:10,
+         position:"right",
+         preload:1,
+         lens:1
+     });
+    function mycarousel_initCallback(carousel){
+        $("#mycarousel li").mouseover(function(){
+            var JQ_img = $("img",this);
+            var image_show = JQ_img.attr("showimg");
+            var image_big = JQ_img.attr("bigimg"); 
+            //$("#_middleImage").attr("src" + "images" + image_show).attr("bigimg" + "images" +  image_big); 
+            $("#_middleImage").attr("src",image_show).attr("bigimg",image_big); 
+            $(this).siblings().each(function(){
+                $(this).removeClass("cur_on");
+            })
+            $(this).addClass("cur_on");
+        })
+    };
+ });
+ 
